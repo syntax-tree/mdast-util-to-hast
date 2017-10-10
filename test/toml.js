abed5e59553825fcbf771f40f37faac289b84511
simov <simeonvelichkov@gmail.com>
2017-10-10 12:39:55 +0300

    Ignore toml

---
+++ test/toml.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var test = require('tape');
+var u = require('unist-builder');
+var to = require('..');
+
+test('TOML', function (t) {
+  t.equal(
+    to(u('toml', 'kilo: "lima"')),
+    null,
+    'should ignore `toml`'
+  );
+
+  t.end();
+});
