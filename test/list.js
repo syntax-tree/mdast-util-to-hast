cb12c28c938cee7fbd42dadc33e513d6d33fc236
Debjeet Biswas <detj@users.noreply.github.com>
2019-02-15 20:19:10 +0530

    Add class to lists with tasks
    * Add `contains-task-list` class to list if list contains at least one task list item.
    
    Fixes #28 
    
    Reviewed-by: Merlijn Vos <merlijn@soverin.net>

--- test/list.js
+++ test/list.js
@@ -35,6 +35,39 @@ test('List', function(t) {
 
   t.deepEqual(
     to(
+      u('list', {ordered: false}, [
+        u('listItem', {checked: true}, [u('paragraph', [u('text', 'todo')])])
+      ])
+    ),
+    u(
+      'element',
+      {tagName: 'ul', properties: {className: ['contains-task-list']}},
+      [
+        u('text', '\n'),
+        u(
+          'element',
+          {tagName: 'li', properties: {className: ['task-list-item']}},
+          [
+            u(
+              'element',
+              {
+                tagName: 'input',
+                properties: {type: 'checkbox', checked: true, disabled: true}
+              },
+              []
+            ),
+            u('text', ' '),
+            u('text', 'todo')
+          ]
+        ),
+        u('text', '\n')
+      ]
+    ),
+    'should identify task list items in unordered lists'
+  )
+
+  t.deepEqual(
+    to(
       u('list', {ordered: true, start: 3}, [
         u('listItem', [u('paragraph', [u('text', 'x-ray')])])
       ])
