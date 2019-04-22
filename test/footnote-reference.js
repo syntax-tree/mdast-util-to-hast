0bae15be9b254e5f4b37ecb7551439f079b5aa3b
Patrick Lehner <1099818+nevik@users.noreply.github.com>
2019-04-22 08:39:16 +0200

    Add footnoteReference test case for explicitly specified label

--- test/footnote-reference.js
+++ test/footnote-reference.js
@@ -6,34 +6,33 @@ var to = require('..')
 
 test('FootnoteReference', function(t) {
   t.deepEqual(
-    to(
-      u('footnoteReference', {
-        identifier: 'alpha'
-      })
-    ),
-    u(
-      'element',
-      {
-        tagName: 'sup',
-        properties: {
-          id: 'fnref-alpha'
-        }
-      },
-      [
-        u(
-          'element',
-          {
-            tagName: 'a',
-            properties: {
-              href: '#fn-alpha',
-              className: ['footnote-ref']
-            }
-          },
-          [u('text', 'alpha')]
-        )
-      ]
-    ),
+    to(u('footnoteReference', {identifier: 'alpha'})),
+    u('element', {tagName: 'sup', properties: {id: 'fnref-alpha'}}, [
+      u(
+        'element',
+        {
+          tagName: 'a',
+          properties: {href: '#fn-alpha', className: ['footnote-ref']}
+        },
+        [u('text', 'alpha')]
+      )
+    ]),
     'should render `footnoteReference`s'
+  )
+
+  t.deepEqual(
+    to(u('footnoteReference', {identifier: 'alpha', label: 'Alpha'})),
+    u('element', {tagName: 'sup', properties: {id: 'fnref-alpha'}}, [
+      u(
+        'element',
+        {
+          tagName: 'a',
+          properties: {href: '#fn-alpha', className: ['footnote-ref']}
+        },
+        [u('text', 'Alpha')]
+      )
+    ]),
+    'should render `footnoteReference`s (#2)'
   )
 
   t.end()
