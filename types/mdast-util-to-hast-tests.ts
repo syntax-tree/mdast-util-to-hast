345e61ceed224ca9b25e8d679d86376f4bcc7619
Remco Haszing <remcohaszing@gmail.com>
2020-04-29 10:19:49 +0200

    Add types
    Closes GH-40.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ types/mdast-util-to-hast-tests.ts
@@ -0,0 +1,43 @@
+import toHast = require('mdast-util-to-hast')
+
+toHast({type: ''})
+toHast({type: ''}, {allowDangerousHtml: true})
+toHast({type: ''}, {commonmark: true})
+toHast(
+  {type: ''},
+  {
+    handlers: {
+      foo(h, node) {
+        return h(node, 'p')
+      }
+    }
+  }
+)
+toHast(
+  {type: ''},
+  {
+    handlers: {
+      foo(h, node) {
+        return h(node, 'p', {foo: 'bar'})
+      }
+    }
+  }
+)
+toHast(
+  {type: ''},
+  {
+    handlers: {
+      foo(h, node) {
+        return h(node, 'p', {}, [{type: ''}])
+      }
+    }
+  }
+)
+toHast(
+  {type: ''},
+  {
+    unknownHandler(h, node) {
+      return h(node, 'div')
+    }
+  }
+)
