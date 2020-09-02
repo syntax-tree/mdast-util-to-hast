15a21f1edcef3b2037951afb428fd5dc6cacbfa0
Titus Wormer <tituswormer@gmail.com>
2020-09-02 12:06:28 +0200

    Fix multiple line endings in text

--- test/text.js
+++ test/text.js
@@ -7,5 +7,11 @@ var to = require('..')
 test('Nodes', function (t) {
   t.deepEqual(to(u('text', 'alpha')), u('text', 'alpha'), 'should map `text`s')
 
+  t.deepEqual(
+    to(u('text', 'alpha \n \n bravo')),
+    u('text', 'alpha\n\nbravo'),
+    'should trim spaces and tabs around eols'
+  )
+
   t.end()
 })
