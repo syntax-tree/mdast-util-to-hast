197247c95c98cf864733f110b7995dbf9c11cfa9
Titus Wormer <tituswormer@gmail.com>
2020-11-13 11:36:37 +0100

    Fix support for `hName` on non-element
    Previously, an mdast text could not result in an element through a `hName`.
    As it’s rather clear that: `{type: 'text', data: {hName: 'span'}}` should turn
    into a span element node, this was fixed.

--- test/text.js
+++ test/text.js
@@ -13,5 +13,51 @@ test('Nodes', function (t) {
     'should trim spaces and tabs around eols'
   )
 
+  t.deepEqual(
+    to(u('text', {data: {hName: 'span'}}, 'charlie')),
+    u('element', {tagName: 'span', properties: {}}, []),
+    'should transform text nodes w/ `hName` to an `element`'
+  )
+
+  t.deepEqual(
+    to(u('text', {data: {hProperties: {className: 'delta'}}}, 'echo')),
+    {type: 'text', value: 'echo'},
+    'should not transform text nodes w/ `hProperties` w/o `hName` to an `element`'
+  )
+
+  t.deepEqual(
+    to(
+      u(
+        'text',
+        {data: {hName: 'span', hProperties: {className: ['foxtrot']}}},
+        'golf'
+      )
+    ),
+    u('element', {tagName: 'span', properties: {className: ['foxtrot']}}, []),
+    'should transform text nodes w/ `hProperties` and `hName` to an `element`'
+  )
+
+  t.deepEqual(
+    to(
+      u('text', {data: {hChildren: [{type: 'text', value: 'hotel'}]}}, 'india')
+    ),
+    {type: 'text', value: 'india'},
+    'should not transform text nodes w/ `hChildren` w/o `hName` to an `element`'
+  )
+
+  t.deepEqual(
+    to(
+      u(
+        'text',
+        {data: {hName: 'span', hChildren: [{type: 'text', value: 'juliett'}]}},
+        'kilo'
+      )
+    ),
+    u('element', {tagName: 'span', properties: {}}, [
+      {type: 'text', value: 'juliett'}
+    ]),
+    'should transform text nodes w/ `hChildren` and `hName` to an `element`'
+  )
+
   t.end()
 })
