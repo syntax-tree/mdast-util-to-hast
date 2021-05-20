a100cff62a2ebc0c9bb640b71cee9d54da90cef0
Titus Wormer <tituswormer@gmail.com>
2021-05-20 10:33:19 +0200

    Add exports of a couple of types

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').Handler} Handler
+ * @typedef {import('./lib/index.js').Handlers} Handlers
+ * @typedef {import('./lib/index.js').H} H
+ */
+
 export {all} from './lib/all.js'
 export {one} from './lib/one.js'
 export {toHast} from './lib/index.js'
