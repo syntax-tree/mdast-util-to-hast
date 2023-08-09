7ff28fba2310d844ec907a14f36ab388d2ab5b94
Titus Wormer <tituswormer@gmail.com>
2023-08-09 12:04:06 +0200

    Fix trimming of whitespace around breaks
    Related-to: remarkjs/remark#1202.

--- test/break.js
+++ test/break.js
@@ -45,4 +45,36 @@ test('break', async function (t) {
       h('p', ['alpha', h('br'), '\n', h('em', 'bravo')])
     )
   })
+
+  await t.test('should trim text after a `br` (#3)', async function () {
+    assert.deepEqual(
+      toHast({
+        type: 'paragraph',
+        children: [
+          {type: 'text', value: 'a'},
+          {type: 'break'},
+          // U+3000 (ideographic space).
+          {type: 'text', value: '　b'},
+          {type: 'break'},
+          // U+2003 (em space).
+          {type: 'text', value: ' c'},
+          {type: 'break'},
+          // U+00A0 (no-break space).
+          {type: 'text', value: ' d'}
+        ]
+      }),
+      h('p', [
+        'a',
+        h('br'),
+        '\n',
+        '　b',
+        h('br'),
+        '\n',
+        ' c',
+        h('br'),
+        '\n',
+        ' d'
+      ])
+    )
+  })
 })
